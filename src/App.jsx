ed94e403f9c4e651269709874d32b5a10c205ce2
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-10-26 16:46:35 +0300

    Introduction to scss

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Table } from "./Table/Table";
 import { Animation } from "./Animation/Animation";
 import { GridArea } from "./Grid-Area/GridArea";
 import "./Grid-Area/grid-area.css";
+import { Scss } from "./scss/Scss";
+import "./scss/css/main.css";
 export function App() {
   return (
     <div className="min-h-screen ">
       {/*  <Objectfit /> */}
       {/* <Table /> */}
       {/* <Animation /> */}
-      <GridArea />
+      {/* <GridArea /> */}
+      <Scss />
     </div>
   );
 }
---
+++ src/scss/Scss.jsx
@@ -0,0 +1,11 @@
+export function Scss() {
+  return (
+    <div class="container">
+      <div class="box1"></div>
+      <div class="box2"></div>
+      <div class="box3"></div>
+      <div class="box4 deneme"></div>
+      <div class="box4"></div>
+    </div>
+  );
+}
