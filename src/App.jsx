c309cac38344537c8f4d08d72d40ec4fc3bb79a9
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-10-26 12:08:42 +0300

    grid-area

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Objectfit } from "./object-fit/Objectfit";
 import { Table } from "./Table/Table";
 import { Animation } from "./Animation/Animation";
+import { GridArea } from "./Grid-Area/GridArea";
+import "./Grid-Area/grid-area.css";
 export function App() {
   return (
     <div className="min-h-screen ">
       {/*  <Objectfit /> */}
       {/* <Table /> */}
-      <Animation />
+      {/* <Animation /> */}
+      <GridArea />
     </div>
   );
 }
---
+++ src/Grid-Area/GridArea.jsx
@@ -0,0 +1,50 @@
+export function GridArea() {
+  return (
+    <div className="container">
+      <header>
+        <h2>Başlık</h2>
+      </header>
+      <section>
+        <h3>Alt Başlık</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam aliquid
+          porro voluptates minus consectetur, hic blanditiis dolores perferendis
+          est pariatur sequi modi dolorum, quis error nulla consequatur
+          voluptas, officia libero?
+        </p>
+      </section>
+      <aside>
+        <h3>Menü</h3>
+        <ul>
+          <li>
+            <a href="#"> Link 1</a>
+          </li>
+          <li>
+            <a href="#"> Link 2</a>
+          </li>
+          <li>
+            <a href="#"> Link 3</a>
+          </li>
+          <li>
+            <a href="#"> Link 4</a>
+          </li>
+        </ul>
+      </aside>
+      <div className="box-1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta
+        reiciendis doloribus, ad asperiores ut molestias voluptatum vitae est
+        quidem.
+      </div>
+      <div className="box-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta
+        reiciendis doloribus, ad asperiores ut molestias voluptatum vitae est
+        quidem.
+      </div>
+      <div className="box-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta
+        reiciendis doloribus, ad asperiores ut molestias voluptatum vitae est
+        quidem.
+      </div>
+    </div>
+  );
+}
